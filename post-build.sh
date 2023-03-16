a5f1d15212bb5b4ca1f4c649cfd47f7889a770b6
Eric Oestrich <eric@oestrich.org>
2023-03-16 08:20:30 -0400

    Include provisioning configuration similar to rpi images

--- post-build.sh
+++ post-build.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-# Create the revert script for manually switching back to the previously active
-# firmware.
+# Create the revert script for manually switching back to the previously
+# active firmware.
 mkdir -p $TARGET_DIR/usr/share/fwup
 $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_DIR/usr/share/fwup/revert.fw
+
+# Copy the fwup includes to the images dir
+cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
 
 # Since U-Boot's squashfs support is so slow (sadly), store the Linux
 # kernel in the FAT filesystem instead of /boot. Everything else can
